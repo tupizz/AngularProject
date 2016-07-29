d83df00fe6d3d1c948e28e0494bcd6ae6aaf4d13
tupizz <tadeu.tupiz@gmail.com>
2016-07-29 02:19:23 -0300

    29/07 Training

--- src/main/webapp/resources/js/modulos.js
+++ src/main/webapp/resources/js/modulos.js
@@ -1,10 +1,10 @@
-angular.module("listaTelefonica", []);
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope) {
+angular.module("listaTelefonica", ['ngMask']);
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, $filter) {
     $scope.app = "Lista Telefônica";
     $scope.contatos = [
-        {nome: "Pedro", telefone: "99999999"},
-        {nome: "Ana", telefone: "92999999"},
-        {nome: "Maria", telefone: "99999399"}
+        {nome: $filter('uppercase')("Pedro"), telefone: "(11) 9999-9999", data: new Date(), operadora: {nome: "Oi", codigo: "14", categoria: "Celular"}},
+        {nome: $filter('uppercase')("Ana"), telefone: "(11) 9299-9999", data: new Date(), operadora: {nome: "Tim", codigo: "41", categoria: "Celular"}},
+        {nome: $filter('uppercase')("Maria"), telefone: "(11) 9999-9399", data: new Date(), operadora:{nome: "Vivo", codigo: "15", categoria: "Celular"}}
     ];
     $scope.operadoras = [
         {nome: "Oi", codigo: "14", categoria: "Celular"},
@@ -14,8 +14,11 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
         {nome: "Embratel", codigo: "21", categoria: "Fixo"}
     ];
     $scope.adicionarContato = function (contato) {
+        contato.data = new Date();
         $scope.contatos.push(angular.copy(contato));
+        console.log(contato);
         delete $scope.contato;
+        $scope.contatoForm.$setPristine(); //Após adicionar, volta o formulário para o estado inicial dele.
     };
     $scope.apagarContatos = function (contatos) {
         $scope.contatos = contatos.filter(function (contato) {
@@ -27,6 +30,9 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
         return contatos.some(function (contato) {
             return contato.selecionado;
         });
-
+    };
+    $scope.ordenarPor= function (tipo){
+        $scope.criterioDeOrdenacao = tipo;
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
     };
 });
